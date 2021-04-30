9c22d213584f2a01e4e10851063476ab2982fc58
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-30 09:34:14 -0500

    Search works, needs prettification on results page

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
@@ -24,5 +25,14 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value = "results")
+    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+         ArrayList<Job> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+         model.addAttribute("jobs", jobs);
+         model.addAttribute("title", "Search Results");
+         return "search-results";
+
+
+    }
 
 }
